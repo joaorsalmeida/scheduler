6061c68369d8b6d9c833d2132234bed63264dc55
Joao Almeida <joaorsalmeida@gmail.com>
2015-03-07 22:29:59 -0300

    Finding appointments fixed

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -1,5 +1,5 @@
 module ScheduleHelper
   def find_appointment(events, date, hour)
-    events.bsearch { |e| e.date == date && e.time.hour == hour} 
+    events.bsearch { |e| e.date == DateTime.new(date.year, date.month, date.day, hour)}
   end
 end
