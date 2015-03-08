01d4debd1960d1163f18acdd56add0d0e915d7d3
Joao Almeida <joaorsalmeida@gmail.com>
2015-03-07 22:49:52 -0300

    Testing appointment rendering

--- spec/views/schedule/index.html.erb_spec.rb
+++ spec/views/schedule/index.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "schedule/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "view schedule" do
+    it "display appointments" do
+      assign :start_date, DateTime.new(2013,5,1)
+      assign :end_date, DateTime.new(2013, 5, 3)
+      assign :dates, [DateTime.new(2013,5,1), DateTime.new(2013,5,2), DateTime.new(2013,5,3)]
+      assign :events, [Appointment.new({ title: "Titulo", date: DateTime.new(2013,5,2,10,0) })]
+      render
+      expect(rendered).to include "Titulo"
+    end
+  end
 end
