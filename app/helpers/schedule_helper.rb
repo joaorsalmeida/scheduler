dab23ef80845199b6ef0c5048963a2038b5537ba
Joao Almeida <joao.almeida@locaweb.com.br>
2015-03-24 10:55:32 -0300

    creating appointment via ajax

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -2,4 +2,8 @@ module ScheduleHelper
   def find_appointment(events, date, hour)
     events.bsearch { |e| e.date == DateTime.new(date.year, date.month, date.day, hour)}
   end
+
+  def format_date(date)
+    date.strftime('%Y-%m-%dT%H:%M:%S')
+  end
 end
